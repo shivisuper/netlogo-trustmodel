6804d74be4d67eb7de004e7afa8c7fe1279c0e87
shivisuper <shivank.shuman@gmail.com>
2018-08-22 18:55:27 +1000

    remove unnecessary service. use spring bean injection

--- src/main/java/com/netlogo/trustmodel/controllers/FireModelController.java
+++ src/main/java/com/netlogo/trustmodel/controllers/FireModelController.java
@@ -1,24 +1,20 @@
 package com.netlogo.trustmodel.controllers;
 
-import com.netlogo.trustmodel.services.Workspace;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.nlogo.headless.HeadlessWorkspace;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class FireModelController {
     @NonNull
-    private final Workspace workspace;
-
-    @Autowired
-    public FireModelController(Workspace workspace) {
-        this.workspace = workspace;
-    }
+    private final HeadlessWorkspace headlessWorkspace;
 
     @GetMapping("/test/{name}")
     public String test(@PathVariable String name) {
-        return String.format("Hello %s", name);
+        return String.format("Hello %s\nRunning headless: %s", name, headlessWorkspace.isHeadless());
     }
 }
